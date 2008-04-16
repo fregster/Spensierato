0c3ec3ff2740fe2ceeec18a6fa61361de9e9c9b0
(no author) <(no author)@7c760586-4595-4335-ae7b-0470aa6287ea>
2008-04-16 10:04:32 +0000

    Added search JS

--- cms/js/public.js
+++ cms/js/public.js
@@ -1,1 +1,62 @@
 // JavaScript Document
+<!-- AJAX Code for search -->
+var xmlHttp = false;
+function ajaxFunction(url, parameters)
+{
+var xmlHttp;
+try
+  {
+  // Firefox, Opera 8.0+, Safari
+  xmlHttp=new XMLHttpRequest();
+  }
+catch (e)
+  {
+  // Internet Explorer
+  try
+    {
+    xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+    }
+  catch (e)
+    {
+    try
+      {
+      xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+    catch (e)
+      {
+      alert("Your browser does not support AJAX!");
+      //return false;
+      }
+    }
+  }
+   //Test the connection
+   if (!xmlHttp)
+   {
+      alert('Cannot create XMLHTTP instance');
+      //return false;
+   }
+
+    if (xmlHttp.overrideMimeType)
+        {
+                //If we can force the mimetype
+                xmlHttp.overrideMimeType('text/xml');
+        }
+
+        xmlHttp.onreadystatechange=function()
+    {
+        if(xmlHttp.readyState==4)
+                {
+                        document.getElementById('page_section_main').innerHTML = xmlHttp.responseText;
+                }
+    }
+
+        xmlHttp.open('GET', url + parameters, true);
+        xmlHttp.send(null);
+}
+
+function searchFunction()
+{
+        ajaxFunction('http://www.fryer.org.uk/test/nuneaton/tools/search?s=', document.forms["searchform"].elements["searchtext"].value );
+        //+ '&types='+ getCheckedValue(document.forms["searchform"].elements["Types"]
+        document.forms["searchform"].elements["searchtext"].value = '';
+}

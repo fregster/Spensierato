ef6349567a2135e5e874747d7ddf4ef789b5b688
(no author) <(no author)@7c760586-4595-4335-ae7b-0470aa6287ea>
2008-04-17 10:50:25 +0000

    Updated concaternation to use +

--- cms/js/admin.js
+++ cms/js/admin.js
@@ -25,6 +25,6 @@ function ajaxSave() {
 function editContent(id) {
 	//Replace content with textarea which should then get replaced by TinyMCE
 	currentContent = document.getElementById(id);
-	 document.getElementById(id).innerHTML = '<form action="" name="edit_',id,'"><div><textarea>',currentContent,'</textarea></div></form>';
+	 document.getElementById(id).innerHTML = '<form action="" name="edit_'+id+'"><div><textarea>'+currentContent+'</textarea></div></form>';
 
 }

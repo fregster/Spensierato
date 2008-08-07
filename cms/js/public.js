427f28ac10aed87c52e716c2434f665bfbc65cf4
(no author) <(no author)@7c760586-4595-4335-ae7b-0470aa6287ea>
2008-08-07 11:15:32 +0000

    cleaned public JS file to save bandwidth

--- cms/js/public.js
+++ cms/js/public.js
@@ -1,405 +1,329 @@
-function ajaxObject()
-{
-	var http = false;
-	//Use IE's ActiveX items to load the file.
-	if(typeof ActiveXObject != 'undefined') 
-	{
-		try {http = new ActiveXObject("Msxml2.XMLHTTP");}
-		catch (e) 
-		{
-			try {http = new ActiveXObject("Microsoft.XMLHTTP");}
-			catch (E) {http = false;}
-		}
-	//If ActiveX is not available, use the XMLHttpRequest of Firefox/Mozilla etc. to load the document.
-	} else if (XMLHttpRequest) {
-		try {http = new XMLHttpRequest();}
-		catch (ee) {http = false;}
-	}
-	
-	return http;
-};
-
-var http = ajaxObject();
-
-function ajaxGet(url, parameters)
-{
-	http.open('GET', url + parameters, true);
-	http.send(null);
-};
-
-function ajaxPost(url, parameters)
-{
-		http.open('POST', url, true);
-		//Send the proper header information along with the request
-		http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		http.setRequestHeader("Content-length", parameters.length);
-		http.setRequestHeader("Connection", "close");
-		http.send(parameters);
-};
-
-function ajaxGetXML()
-{
-	//IE does not return responseXML
-	var xmldoc;
-	if(window.ActiveXObject)
-	{
-		xmldoc = new ActiveXObject("Microsoft.XMLDOM");
-		xmldoc.async="false";
-		xmldoc.loadXML(http.responseText);
-	}
-	else
-	{
-		xmldoc = http.responseXML;
-	}
-	
-	return xmldoc;
-};
-
-function searchFunction()
-{		
-        ajaxGet(ajax_host+'/tools/search?s=', document.forms.searchform.elements.searchtext.value );
-        document.forms.searchform.elements.searchtext.value = '';
-        
-        http.onreadystatechange=function()
-    	{
-        	if(http.readyState==4)
-			{
-			        document.getElementById('page_section_main').innerHTML = http.responseText;
-			}
-		};
-};
-
-
-var ToolTips = new Tips($$('.ToolTip'), {
-	className: 'ToolTip'
-});
-
-var SpriteTips = new Tips($$('.sprite'), {
-	className: 'ToolTip'
-});
-
-function pageReload(returnVal) 
-{
-	setTimeout('window.top.location.reload(true)',100);
-};
-
-function addLoadEvent(func)
-{
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function')
-	{
-		window.onload = func;
-		} 
-		else 
-		{
-			window.onload = function(){
-			oldonload();
-			func();
-		}
-	}
+function ajaxObject(){
+var _1=false;
+if(typeof ActiveXObject!="undefined"){
+try{
+_1=new ActiveXObject("Msxml2.XMLHTTP");
 }
-
+catch(e){
+try{
+_1=new ActiveXObject("Microsoft.XMLHTTP");
+}
+catch(E){
+_1=false;
+}
+}
+}else{
+if(XMLHttpRequest){
+try{
+_1=new XMLHttpRequest();
+}
+catch(ee){
+_1=false;
+}
+}
+}
+return _1;
+};
+var http=ajaxObject();
+function ajaxGet(_2,_3){
+http.open("GET",_2+_3,true);
+http.send(null);
+};
+function ajaxPost(_4,_5){
+http.open("POST",_4,true);
+http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+http.setRequestHeader("Content-length",_5.length);
+http.setRequestHeader("Connection","close");
+http.send(_5);
+};
+function ajaxGetXML(){
+var _6;
+if(window.ActiveXObject){
+_6=new ActiveXObject("Microsoft.XMLDOM");
+_6.async="false";
+_6.loadXML(http.responseText);
+}else{
+_6=http.responseXML;
+}
+return _6;
+};
+function searchFunction(){
+ajaxGet(ajax_host+"/tools/search?s=",document.forms.searchform.elements.searchtext.value);
+document.forms.searchform.elements.searchtext.value="";
+http.onreadystatechange=function(){
+if(http.readyState==4){
+document.getElementById("page_section_main").innerHTML=http.responseText;
+}
+};
+};
+var ToolTips=new Tips($$(".ToolTip"),{className:"ToolTip"});
+var SpriteTips=new Tips($$(".sprite"),{className:"ToolTip"});
+function pageReload(_7){
+setTimeout("window.top.location.reload(true)",100);
+};
+function addLoadEvent(_8){
+var _9=window.onload;
+if(typeof window.onload!="function"){
+window.onload=_8;
+}else{
+window.onload=function(){
+_9();
+_8();
+};
+}
+};
 var notes;
-//Init scripts to auto load stuff
 var initalised;
 var SmoothScroll;
-function init()
-{ 
-	if(initalised != true)
-	{
-		//SmoothScroll = new SmoothScroll({duration: 1200});	//Load the smooth scroller
-		stepFontSize(readCookie('fontSize')); //Set the font size
-		
-		notes = $('notifications');
-		notes.fade.bind(notes, [0]);
-		rounded();
-		initalised = true;		
-	}
+function init(){
+if(initalised!=true){
+stepFontSize(readCookie("fontSize"));
+notes=$("notifications");
+notes.fade.bind(notes,[0]);
+rounded();
+initalised=true;
+}
 };
-
 addLoadEvent(init);
-
-function notification(text, colour, font)
-{
-	var timer;
-	if(!font) { 
-		var font = "black";
-	}
-	
-	document.getElementById('notifications').style.backgroundColor = colour;
-	document.getElementById('notifications').style.font = font;
-	document.getElementById('notifications').innerHTML = text;
-	
-	notes.fade(-0, 0.8);
-	
-	//After 3 seconds fade back out
-	timer=setTimeout('notes.fade(0.8, 0)',3000);
-	
-	return false;
-};
-
-function increaseFontSize() {
-   stepFontSize(1);
-};
-function decreaseFontSize() {
-	stepFontSize(-1);  
-};
-
-function stepFontSize(increment) {
-	var p = document.getElementsByTagName('p');
-	var fontSizes = new Array("xx-small", "x-small", "small", "medium", "large", "x-large", "xx-large");
-	var s = 3;
-	var increment = parseInt(increment);
-	
-	for(i=0;i<p.length;i++) {
-      if(p[i].style.fontSize) {
-        
-         for(f=0;f<fontSizes.length;f++) {
-         	if(p[i].style.fontSize == fontSizes[f]) { var s = f; }
-         }
-      }
-      
-      var s = s + increment;
-      
-      if(s < 0) { s = 0; }
-      if(s >= fontSizes.length) { s = fontSizes.length -1; } 
-
-		p[i].style.fontSize = fontSizes[s];
-          
-		//Save the font size in a cookie
-		createCookie('fontSize',s-Math.floor(fontSizes.length / 2),'365');
-   }
-};
-
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path="+document_root;
-};
-
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-};
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-};
-
-function pageReload()
-{
-	window.location.href = unescape(window.location.pathname);
-};
-
-function NiftyCheck() {
-  if(!document.getElementById || !document.createElement) {
-    return false;
-  }
-  var b = navigator.userAgent.toLowerCase();
-  if (b.indexOf("msie 5") > 0 && b.indexOf("opera") == -1) {
-    return false;
-  }
-  return true;
+function notification(_a,_b,_c){
+var _d;
+if(!_c){
+var _c="black";
 }
-
-function rounded() {
-	var bk;
-	if (!NiftyCheck()) return;
-	var v = getElements("rounded");
-	var l = v.length;
-	for (var i = 0; i < l; i++) 
-	{
-		sizex = 4;
-		sizey = 4;
-		sizex_b = 4;
-		sizey_b = 4;
-		
-		// Parse out optional width and height from className
-		var startIndex = v[i].className.indexOf("rounded");
-		var endIndex = v[i].className.indexOf(" ", startIndex);
-		if (endIndex < 0) {
-			endIndex = v[i].className.length;
-		}
-		var clazz = v[i].className.substring(startIndex, endIndex);
-		params = clazz.split('-');
-		
-		if(params.length > 1) {
-			sizex = parseInt(params[1]);
-			sizey = parseInt(params[2]);
-		
-			if (params.length >3) {
-				sizex_b = parseInt(params[3]);
-				sizey_b = parseInt(params[4]);
-			}
-		}
-
-		color = get_current_style(v[i],"background-color","transparent");
-		bk = get_current_style(v[i].parentNode,"background-color","transparent");
-		AddRounded(v[i], bk, color, sizex, sizey, true);
-		AddRounded(v[i], bk, color, sizex_b, sizey_b, false);
-	}
+document.getElementById("notifications").style.backgroundColor=_b;
+document.getElementById("notifications").style.font=_c;
+document.getElementById("notifications").innerHTML=_a;
+notes.fade(-0,0.8);
+_d=setTimeout("notes.fade(0.8, 0)",3000);
+return false;
+};
+function increaseFontSize(){
+stepFontSize(1);
+};
+function decreaseFontSize(){
+stepFontSize(-1);
+};
+function stepFontSize(_e){
+var p=document.getElementsByTagName("p");
+var _10=new Array("xx-small","x-small","small","medium","large","x-large","xx-large");
+var s=3;
+var _e=parseInt(_e);
+for(i=0;i<p.length;i++){
+if(p[i].style.fontSize){
+for(f=0;f<_10.length;f++){
+if(p[i].style.fontSize==_10[f]){
+var s=f;
 }
-
-function mathsquare(x) {
-  return x*x;
+}
+}
+var s=s+_e;
+if(s<0){
+s=0;
+}
+if(s>=_10.length){
+s=_10.length-1;
+}
+p[i].style.fontSize=_10[s];
+createCookie("fontSize",s-Math.floor(_10.length/2),"365");
+}
+};
+function createCookie(_12,_13,_14){
+if(_14){
+var _15=new Date();
+_15.setTime(_15.getTime()+(_14*24*60*60*1000));
+var _16="; expires="+_15.toGMTString();
+}else{
+var _16="";
+}
+document.cookie=_12+"="+_13+_16+"; path="+document_root;
+};
+function readCookie(_17){
+var _18=_17+"=";
+var ca=document.cookie.split(";");
+for(var i=0;i<ca.length;i++){
+var c=ca[i];
+while(c.charAt(0)==" "){
+c=c.substring(1,c.length);
+}
+if(c.indexOf(_18)==0){
+return c.substring(_18.length,c.length);
+}
+}
+return null;
+};
+function eraseCookie(_1c){
+createCookie(_1c,"",-1);
+};
+function pageReload(){
+window.location.href=unescape(window.location.pathname);
+};
+function NiftyCheck(){
+if(!document.getElementById||!document.createElement){
+return false;
+}
+var b=navigator.userAgent.toLowerCase();
+if(b.indexOf("msie 5")>0&&b.indexOf("opera")==-1){
+return false;
+}
+return true;
+};
+function rounded(){
+var bk;
+if(!NiftyCheck()){
+return;
+}
+var v=getElements("rounded");
+var l=v.length;
+for(var i=0;i<l;i++){
+sizex=4;
+sizey=4;
+sizex_b=4;
+sizey_b=4;
+var _22=v[i].className.indexOf("rounded");
+var _23=v[i].className.indexOf(" ",_22);
+if(_23<0){
+_23=v[i].className.length;
+}
+var _24=v[i].className.substring(_22,_23);
+params=_24.split("-");
+if(params.length>1){
+sizex=parseInt(params[1]);
+sizey=parseInt(params[2]);
+if(params.length>3){
+sizex_b=parseInt(params[3]);
+sizey_b=parseInt(params[4]);
+}
+}
+color=get_current_style(v[i],"background-color","transparent");
+bk=get_current_style(v[i].parentNode,"background-color","transparent");
+AddRounded(v[i],bk,color,sizex,sizey,true);
+AddRounded(v[i],bk,color,sizex_b,sizey_b,false);
+}
+};
+function mathsquare(x){
+return x*x;
+};
+function Blend(a,b,_28){
+var ca=Array(parseInt("0x"+a.substring(1,3)),parseInt("0x"+a.substring(3,5)),parseInt("0x"+a.substring(5,7)));
+var cb=Array(parseInt("0x"+b.substring(1,3)),parseInt("0x"+b.substring(3,5)),parseInt("0x"+b.substring(5,7)));
+return "#"+("0"+Math.round(ca[0]+(cb[0]-ca[0])*_28).toString(16)).slice(-2).toString(16)+("0"+Math.round(ca[1]+(cb[1]-ca[1])*_28).toString(16)).slice(-2).toString(16)+("0"+Math.round(ca[2]+(cb[2]-ca[2])*_28).toString(16)).slice(-2).toString(16);
+return "#"+("0"+Math.round(ca[0]+(cb[0]-ca[0])*_28).toString(16)).slice(-2).toString(16)+("0"+Math.round(ca[1]+(cb[1]-ca[1])*_28).toString(16)).slice(-2).toString(16)+("0"+Math.round(ca[2]+(cb[2]-ca[2])*_28).toString(16)).slice(-2).toString(16);
+};
+function AddRounded(el,bk,_2d,_2e,_2f,top){
+if(!_2e&&!_2f){
+return;
+}
+var i,j;
+var d=document.createElement("div");
+d.style.backgroundColor=bk;
+var _34=0;
+for(i=1;i<=_2f;i++){
+var _35,_36,_37;
+arc=Math.sqrt(1-mathsquare(1-i/_2f))*_2e;
+var _38=_2e-Math.ceil(arc);
+var _39=Math.floor(_34);
+var _3a=_2e-_38-_39;
+var x=document.createElement("div");
+var y=d;
+x.style.margin="0px "+_38+"px";
+x.style.height="1px";
+x.style.overflow="hidden";
+for(j=1;j<=_3a;j++){
+if(j==1){
+if(j==_3a){
+_35=((arc+_34)*0.5)-_39;
+}else{
+_36=Math.sqrt(1-mathsquare((_2e-_38-j+1)/_2e))*_2f;
+_35=(_36-(_2f-i))*(arc-_39-_3a+1)*0.5;
+_35=0;
+}
+}else{
+if(j==_3a){
+_36=Math.sqrt(1-mathsquare((_2e-_38-j+1)/_2e))*_2f;
+_35=1-(1-(_36-(_2f-i)))*(1-(_34-_39))*0.5;
+}else{
+_37=Math.sqrt(1-mathsquare((_2e-_38-j)/_2e))*_2f;
+_36=Math.sqrt(1-mathsquare((_2e-_38-j+1)/_2e))*_2f;
+_35=((_36+_37)*0.5)-(_2f-i);
+}
+}
+x.style.backgroundColor=Blend(bk,_2d,_35);
+if(top){
+y.appendChild(x);
+}else{
+y.insertBefore(x,y.firstChild);
+}
+y=x;
+var x=document.createElement("div");
+x.style.height="1px";
+x.style.overflow="hidden";
+x.style.margin="0px 1px";
+}
+x.style.backgroundColor=_2d;
+if(top){
+y.appendChild(x);
+}else{
+y.insertBefore(x,y.firstChild);
+}
+_34=arc;
+}
+if(top){
+el.insertBefore(d,el.firstChild);
+}else{
+el.appendChild(d);
+}
+};
+function getElements(_3d){
+var _3e=[];
+var el=document.getElementsByTagName("DIV");
+var _40=new RegExp("\\b"+_3d+"\\b");
+for(var i=0;i<el.length;i++){
+if(_40.test(el[i].className)){
+_3e.push(el[i]);
+}
+}
+return _3e;
+};
+function get_current_style(_42,_43,_44){
+var ee,i,val,apr;
+try{
+var cs=document.defaultView.getComputedStyle(_42,"");
+val=cs.getPropertyValue(_43);
+}
+catch(ee){
+if(_42.currentStyle){
+apr=_43.split("-");
+for(i=1;i<apr.length;i++){
+apr[i]=apr[i].toUpperCase();
+}
+apr=apr.join("");
+val=_42.currentStyle.getAttribute(apr);
+}
+}
+if((val.indexOf("rgba")>-1||val==_44)&&_42.parentNode){
+if(_42.parentNode!=document){
+val=get_current_style(_42.parentNode,_43,_44);
+}else{
+val="#FFFFFF";
+}
+}
+if(val.indexOf("rgb")>-1&&val.indexOf("rgba")==-1){
+val=rgb2hex(val);
+}
+if(val.length==4){
+val="#"+val.substring(1,1)+val.substring(1,1)+val.substring(2,1)+val.substring(2,1)+val.substring(3,1)+val.substring(3,1);
+}
+return val;
+};
+function rgb2hex(_4a){
+var x=255;
+var hex="";
+var i;
+var _4e=/([0-9]+)[, ]+([0-9]+)[, ]+([0-9]+)/;
+var _4f=_4e.exec(_4a);
+for(i=1;i<4;i++){
+hex+=("0"+parseInt(_4f[i]).toString(16)).slice(-2);
+}
+return "#"+hex;
 };
 
-function Blend(a, b, alpha) {
-
-  var ca = Array(
-    parseInt('0x' + a.substring(1, 3)), 
-    parseInt('0x' + a.substring(3, 5)), 
-    parseInt('0x' + a.substring(5, 7))
-  );
-  var cb = Array(
-    parseInt('0x' + b.substring(1, 3)), 
-    parseInt('0x' + b.substring(3, 5)), 
-    parseInt('0x' + b.substring(5, 7))
-  );
-  return '#' + ('0'+Math.round(ca[0] + (cb[0] - ca[0])*alpha).toString(16)).slice(-2).toString(16)
-             + ('0'+Math.round(ca[1] + (cb[1] - ca[1])*alpha).toString(16)).slice(-2).toString(16)
-             + ('0'+Math.round(ca[2] + (cb[2] - ca[2])*alpha).toString(16)).slice(-2).toString(16);
-
-  return '#' + ('0'+Math.round(ca[0] + (cb[0] - ca[0])*alpha).toString(16)).slice(-2).toString(16)
-             + ('0'+Math.round(ca[1] + (cb[1] - ca[1])*alpha).toString(16)).slice(-2).toString(16)
-             + ('0'+Math.round(ca[2] + (cb[2] - ca[2])*alpha).toString(16)).slice(-2).toString(16);
-}
-
-function AddRounded(el, bk, color, sizex, sizey, top) {
-  if (!sizex && !sizey)
-	return;
-  var i, j;
-  var d = document.createElement("div");
-  d.style.backgroundColor = bk;
-  var lastarc = 0;
-  for (i = 1; i <= sizey; i++) {
-    var coverage, arc2, arc3;
-    // Find intersection of arc with bottom of pixel row
-    arc = Math.sqrt(1.0 - mathsquare(1.0 - i / sizey)) * sizex;
-    // Calculate how many pixels are bg, fg and blended.
-    var n_bg = sizex - Math.ceil(arc);
-    var n_fg = Math.floor(lastarc);
-    var n_aa = sizex - n_bg - n_fg;
-    // Create pixel row wrapper
-    var x = document.createElement("div");
-    var y = d;
-    x.style.margin = "0px " + n_bg + "px";
-	x.style.height='1px';
-	x.style.overflow='hidden';
-    // Make a wrapper per anti-aliased pixel (at least one)
-    for (j = 1; j <= n_aa; j++) {
-      // Calculate coverage per pixel
-      // (approximates circle by a line within the pixel)
-      if (j == 1) {
-        if (j == n_aa) {
-          // Single pixel
-          coverage = ((arc + lastarc) * .5) - n_fg;
-        }
-        else {
-          // First in a run
-          arc2 = Math.sqrt(1.0 - mathsquare((sizex - n_bg - j + 1) / sizex)) * sizey;
-          coverage = (arc2 - (sizey - i)) * (arc - n_fg - n_aa + 1) * .5;
-          // Coverage is incorrect. Why?
-          coverage = 0;
-        }
-      }
-      else if (j == n_aa) {
-        // Last in a run
-        arc2 = Math.sqrt(1.0 - mathsquare((sizex - n_bg - j + 1) / sizex)) * sizey;
-        coverage = 1.0 - (1.0 - (arc2 - (sizey - i))) * (1.0 - (lastarc - n_fg)) * .5;
-      }
-      else {
-        // Middle of a run
-        arc3 = Math.sqrt(1.0 - mathsquare((sizex - n_bg - j) / sizex)) * sizey;
-        arc2 = Math.sqrt(1.0 - mathsquare((sizex - n_bg - j + 1) / sizex)) * sizey;
-        coverage = ((arc2 + arc3) * .5) - (sizey - i);
-      }
-      
-      x.style.backgroundColor = Blend(bk, color, coverage);
-	  if (top)
-	      y.appendChild(x);
-      else
-	      y.insertBefore(x, y.firstChild);
-      y = x;
-      var x = document.createElement("div");
-		x.style.height='1px';
-		x.style.overflow='hidden';
-      x.style.margin = "0px 1px";
-    }
-    x.style.backgroundColor = color;
-    if (top)
-	    y.appendChild(x);
-    else
-		y.insertBefore(x, y.firstChild);
-    lastarc = arc;
-  }
-  if (top)
-	  el.insertBefore(d, el.firstChild);
-  else
-	  el.appendChild(d);
-}
-
-function getElements(className) {
-	var elements = [];
-	var el = document.getElementsByTagName('DIV');  
-	var regexp=new RegExp("\\b"+className+"\\b");
-	for (var i = 0; i < el.length; i++) 
-	{
-		if (regexp.test(el[i].className)) 
-			elements.push(el[i]);
-	}
-	return elements;
-}
-
-function get_current_style(element,property,not_accepted)
-{
-  var ee,i,val,apr;
-  try
-  {
-    var cs=document.defaultView.getComputedStyle(element,'');
-    val=cs.getPropertyValue(property);
-  }
-  catch(ee)
-  {
-    if(element.currentStyle)
-  	{
-	    apr=property.split("-");
-	    for(i=1;i<apr.length;i++) apr[i]=apr[i].toUpperCase();
-	    apr=apr.join("");
-	    val=element.currentStyle.getAttribute(apr);
-   }
-  }
-  if((val.indexOf("rgba") > -1 || val==not_accepted) && element.parentNode)
-  {
-	 if(element.parentNode != document) 
-		 val=get_current_style(element.parentNode,property,not_accepted);
-	 else
-		 val = '#FFFFFF';
-  }
-  if (val.indexOf("rgb") > -1 && val.indexOf("rgba") == -1)
-	  val = rgb2hex(val);
-  if (val.length == 4)
-	  val = '#'+val.substring(1,1)+val.substring(1,1)+val.substring(2,1)+val.substring(2,1)+val.substring(3,1)+val.substring(3,1);
-  return val;
-}
-
-function rgb2hex(value)
-{
-	var x = 255;
-	var hex = '';
-	var i;
-	var regexp=/([0-9]+)[, ]+([0-9]+)[, ]+([0-9]+)/;
-	var array=regexp.exec(value);
-	for(i=1;i<4;i++) hex += ('0'+parseInt(array[i]).toString(16)).slice(-2);
-	return '#'+hex;
-}

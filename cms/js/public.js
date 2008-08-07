5ccc211c618fa6ec08b9fc01c67e97de2b2c1ab8
(no author) <(no author)@7c760586-4595-4335-ae7b-0470aa6287ea>
2008-08-07 08:28:14 +0000

    Update to move login JS to only load on login page to save bandwidth

--- cms/js/public.js
+++ cms/js/public.js
@@ -178,135 +178,197 @@ function eraseCookie(name) {
 	createCookie(name,"",-1);
 };
 
-function jsSecureLogin() {
-	var username = document.forms.login.elements.username.value;
-	var password = document.forms.login.elements.password.value;
-	var code = document.forms.login.elements.security_code.value;
-	var hash = SHA256(SHA256(password)+code);	
-	
-	ajaxGet(ajax_host+'/ajax/login?', 'username='+username+'&key='+hash);
-    document.forms.searchform.elements.searchtext.value = '';
-       
-    http.onreadystatechange=function()
-   	{
-       	if(http.readyState==4)
-		{
-			if(http.responseText=='true')
-			{
-				window.location = document_root+'/'+current_page;
-			}
-			else
-			{
-				pageReload()
-			}
-		}
-	};
-	
-};
-
 function pageReload()
 {
 	window.location.href = unescape(window.location.pathname);
 };
 
-/**
-*
-*  Secure Hash Algorithm (SHA256)
-*  http://www.webtoolkit.info/
-*
-*  Original code by Angel Marin, Paul Johnston.
-*
-**/
 
-function SHA256(s){
 
-    var chrsz   = 8;
-    var hexcase = 0;
+// Contributors 
+// Ilkka Huotari at http://www.editsite.net
+// Mathieu 'p01' HENRI at http://www.p01.org/
+// http://seky.nahory.net/2005/04/rounded-corners/
+// Steven Wittens at http://www.acko.net/anti-aliased-nifty-corners
+// Original Nifty Corners by Alessandro Fulciniti at http://pro.html.it/esempio/nifty/
+function NiftyCheck() {
+  if(!document.getElementById || !document.createElement) {
+    return false;
+  }
+  var b = navigator.userAgent.toLowerCase();
+  if (b.indexOf("msie 5") > 0 && b.indexOf("opera") == -1) {
+    return false;
+  }
+  return true;
+}
 
-    function safe_add (x, y) {
-        var lsw = (x & 0xFFFF) + (y & 0xFFFF);
-        var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
-        return (msw << 16) | (lsw & 0xFFFF);
-    }
+function Rounded(className, sizex, sizey, sizex_b, sizey_b) {
+	var bk;
+	if (!NiftyCheck()) return;
+	if (typeof(sizex_b) == 'undefined')
+		sizex_b = sizex;
+	if (typeof(sizey_b) == 'undefined')
+		sizey_b = sizey;
+	var v = getElements(className);
+	var l = v.length;
+	for (var i = 0; i < l; i++) {
+		color = get_current_style(v[i],"background-color","transparent");
+		bk = get_current_style(v[i].parentNode,"background-color","transparent");
+		AddRounded(v[i], bk, color, sizex, sizey, true);
+		AddRounded(v[i], bk, color, sizex_b, sizey_b, false);
+	}
+}
 
-    function S (X, n) { return ( X >>> n ) | (X << (32 - n)); }
-    function R (X, n) { return ( X >>> n ); }
-    function Ch(x, y, z) { return ((x & y) ^ ((~x) & z)); }
-    function Maj(x, y, z) { return ((x & y) ^ (x & z) ^ (y & z)); }
-    function Sigma0256(x) { return (S(x, 2) ^ S(x, 13) ^ S(x, 22)); }
-    function Sigma1256(x) { return (S(x, 6) ^ S(x, 11) ^ S(x, 25)); }
-    function Gamma0256(x) { return (S(x, 7) ^ S(x, 18) ^ R(x, 3)); }
-    function Gamma1256(x) { return (S(x, 17) ^ S(x, 19) ^ R(x, 10)); }
+Math.sqr = function (x) {
+  return x*x;
+};
 
-    function core_sha256 (m, l) {
-        var K = new Array(0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786, 0xFC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA, 0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x6CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070, 0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2);
-        var HASH = new Array(0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19);
-        var W = new Array(64);
-        var a, b, c, d, e, f, g, h, i, j;
-        var T1, T2;
+function Blend(a, b, alpha) {
 
-        m[l >> 5] |= 0x80 << (24 - l % 32);
-        m[((l + 64 >> 9) << 4) + 15] = l;
+  var ca = Array(
+    parseInt('0x' + a.substring(1, 3)), 
+    parseInt('0x' + a.substring(3, 5)), 
+    parseInt('0x' + a.substring(5, 7))
+  );
+  var cb = Array(
+    parseInt('0x' + b.substring(1, 3)), 
+    parseInt('0x' + b.substring(3, 5)), 
+    parseInt('0x' + b.substring(5, 7))
+  );
+  return '#' + ('0'+Math.round(ca[0] + (cb[0] - ca[0])*alpha).toString(16)).slice(-2).toString(16)
+             + ('0'+Math.round(ca[1] + (cb[1] - ca[1])*alpha).toString(16)).slice(-2).toString(16)
+             + ('0'+Math.round(ca[2] + (cb[2] - ca[2])*alpha).toString(16)).slice(-2).toString(16);
 
-        for ( var i = 0; i<m.length; i+=16 ) {
-            a = HASH[0]; b = HASH[1]; c = HASH[2]; d = HASH[3]; e = HASH[4]; f = HASH[5]; g = HASH[6]; h = HASH[7];
+  return '#' + ('0'+Math.round(ca[0] + (cb[0] - ca[0])*alpha).toString(16)).slice(-2).toString(16)
+             + ('0'+Math.round(ca[1] + (cb[1] - ca[1])*alpha).toString(16)).slice(-2).toString(16)
+             + ('0'+Math.round(ca[2] + (cb[2] - ca[2])*alpha).toString(16)).slice(-2).toString(16);
+}
 
-            for ( var j = 0; j<64; j++) {
-                if (j < 16) W[j] = m[j + i];
-                else W[j] = safe_add(safe_add(safe_add(Gamma1256(W[j - 2]), W[j - 7]), Gamma0256(W[j - 15])), W[j - 16]);
-
-                T1 = safe_add(safe_add(safe_add(safe_add(h, Sigma1256(e)), Ch(e, f, g)), K[j]), W[j]);
-                T2 = safe_add(Sigma0256(a), Maj(a, b, c));
-
-                h = g; g = f; f = e;  e = safe_add(d, T1); d = c; c = b; b = a; a = safe_add(T1, T2);
-            }
-
-            HASH[0] = safe_add(a, HASH[0]); HASH[1] = safe_add(b, HASH[1]); HASH[2] = safe_add(c, HASH[2]); HASH[3] = safe_add(d, HASH[3]); HASH[4] = safe_add(e, HASH[4]); HASH[5] = safe_add(f, HASH[5]); HASH[6] = safe_add(g, HASH[6]); HASH[7] = safe_add(h, HASH[7]);
-          }
-        return HASH;
-    }
-
-    function str2binb (str) {
-        var bin = Array();
-        var mask = (1 << chrsz) - 1;
-        for(var i = 0; i < str.length * chrsz; i += chrsz) {
-            bin[i>>5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i%32);
+function AddRounded(el, bk, color, sizex, sizey, top) {
+  if (!sizex && !sizey)
+	return;
+  var i, j;
+  var d = document.createElement("div");
+  d.style.backgroundColor = bk;
+  var lastarc = 0;
+  for (i = 1; i <= sizey; i++) {
+    var coverage, arc2, arc3;
+    // Find intersection of arc with bottom of pixel row
+    arc = Math.sqrt(1.0 - Math.sqr(1.0 - i / sizey)) * sizex;
+    // Calculate how many pixels are bg, fg and blended.
+    var n_bg = sizex - Math.ceil(arc);
+    var n_fg = Math.floor(lastarc);
+    var n_aa = sizex - n_bg - n_fg;
+    // Create pixel row wrapper
+    var x = document.createElement("div");
+    var y = d;
+    x.style.margin = "0px " + n_bg + "px";
+	x.style.height='1px';
+	x.style.overflow='hidden';
+    // Make a wrapper per anti-aliased pixel (at least one)
+    for (j = 1; j <= n_aa; j++) {
+      // Calculate coverage per pixel
+      // (approximates circle by a line within the pixel)
+      if (j == 1) {
+        if (j == n_aa) {
+          // Single pixel
+          coverage = ((arc + lastarc) * .5) - n_fg;
         }
-        return bin;
-    }
-
-    function Utf8Encode(string) {
-        string = string.replace(/\r\n/g,"\n");
-        var utftext = "";
-        for (var n = 0; n < string.length; n++) {
-            var c = string.charCodeAt(n);
-            if (c < 128) {
-                utftext += String.fromCharCode(c);
-            }
-            else if((c > 127) && (c < 2048)) {
-                utftext += String.fromCharCode((c >> 6) | 192);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-            else {
-                utftext += String.fromCharCode((c >> 12) | 224);
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
+        else {
+          // First in a run
+          arc2 = Math.sqrt(1.0 - Math.sqr((sizex - n_bg - j + 1) / sizex)) * sizey;
+          coverage = (arc2 - (sizey - i)) * (arc - n_fg - n_aa + 1) * .5;
+          // Coverage is incorrect. Why?
+          coverage = 0;
         }
-
-        return utftext;
+      }
+      else if (j == n_aa) {
+        // Last in a run
+        arc2 = Math.sqrt(1.0 - Math.sqr((sizex - n_bg - j + 1) / sizex)) * sizey;
+        coverage = 1.0 - (1.0 - (arc2 - (sizey - i))) * (1.0 - (lastarc - n_fg)) * .5;
+      }
+      else {
+        // Middle of a run
+        arc3 = Math.sqrt(1.0 - Math.sqr((sizex - n_bg - j) / sizex)) * sizey;
+        arc2 = Math.sqrt(1.0 - Math.sqr((sizex - n_bg - j + 1) / sizex)) * sizey;
+        coverage = ((arc2 + arc3) * .5) - (sizey - i);
+      }
+      
+      x.style.backgroundColor = Blend(bk, color, coverage);
+	  if (top)
+	      y.appendChild(x);
+      else
+	      y.insertBefore(x, y.firstChild);
+      y = x;
+      var x = document.createElement("div");
+		x.style.height='1px';
+		x.style.overflow='hidden';
+      x.style.margin = "0px 1px";
     }
+    x.style.backgroundColor = color;
+    if (top)
+	    y.appendChild(x);
+    else
+		y.insertBefore(x, y.firstChild);
+    lastarc = arc;
+  }
+  if (top)
+	  el.insertBefore(d, el.firstChild);
+  else
+	  el.appendChild(d);
+}
 
-    function binb2hex (binarray) {
-        var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
-        var str = "";
-        for(var i = 0; i < binarray.length * 4; i++) {
-            str += hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8+4)) & 0xF) +
-            hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8  )) & 0xF);
-        }
-        return str;
-    }
+function getElements(className) {
+	var elements = [];
+	var el = document.getElementsByTagName('DIV');  
+	var regexp=new RegExp("\\b"+className+"\\b");
+	for (var i = 0; i < el.length; i++) 
+	{
+		if (regexp.test(el[i].className)) 
+			elements.push(el[i]);
+	}
+	return elements;
+}
 
-    s = Utf8Encode(s);
-    return binb2hex(core_sha256(str2binb(s), s.length * chrsz));
+function get_current_style(element,property,not_accepted)
+{
+  var ee,i,val,apr;
+  try
+  {
+    var cs=document.defaultView.getComputedStyle(element,'');
+    val=cs.getPropertyValue(property);
+  }
+  catch(ee)
+  {
+    if(element.currentStyle)
+  	{
+	    apr=property.split("-");
+	    for(i=1;i<apr.length;i++) apr[i]=apr[i].toUpperCase();
+	    apr=apr.join("");
+	    val=element.currentStyle.getAttribute(apr);
+   }
+  }
+  if((val.indexOf("rgba") > -1 || val==not_accepted) && element.parentNode)
+  {
+	 if(element.parentNode != document) 
+		 val=get_current_style(element.parentNode,property,not_accepted);
+	 else
+		 val = '#FFFFFF';
+  }
+  if (val.indexOf("rgb") > -1 && val.indexOf("rgba") == -1)
+	  val = rgb2hex(val);
+  if (val.length == 4)
+	  val = '#'+val.substring(1,1)+val.substring(1,1)+val.substring(2,1)+val.substring(2,1)+val.substring(3,1)+val.substring(3,1);
+  return val;
+}
+
+function rgb2hex(value)
+{
+	var x = 255;
+	var hex = '';
+	var i;
+	var regexp=/([0-9]+)[, ]+([0-9]+)[, ]+([0-9]+)/;
+	var array=regexp.exec(value);
+	for(i=1;i<4;i++) hex += ('0'+parseInt(array[i]).toString(16)).slice(-2);
+	return '#'+hex;
 }

0f94820108f71bcf72341b0e96e5090f537c0fba
Fregster <Fregster@7c760586-4595-4335-ae7b-0470aa6287ea>
2008-08-19 14:05:43 +0000

    Added Licence information to the index file

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
+/**
+* Spensierato
+* Content Managment System
+*
+* Requirements: PHP5.2, SimpleXML, Tidy, MDB2 with MySQLi, XML/Serializer, PHP xsltProcessor
+*
+* Copyright (c) 2008 Paul Fryer (www.fryer.org.uk)
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 3 or any latter version of the license.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+*
+* @category CMS
+* @package Spensierato
+* @author Paul Fryer <paul@fryer.org.uk>
+* @license http://www.opensource.org/licenses/gpl-3.0.html GPL
+* @link http://spensierato.fryer.org.uk/
+*/
+
+
 require_once('./spensierato_cms.inc');
 ?>

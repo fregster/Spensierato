453acc3d42e34b0ab8e2467506d734a4d6a66ec4
paul <paul@7c760586-4595-4335-ae7b-0470aa6287ea>
2009-07-16 20:16:10 +0000

    Outline Site structuring tool Skin updates new tags class Search and security now use a common method for getting array values

--- cms/js/folder_tree/drag-drop-folder-tree.js
+++ cms/js/folder_tree/drag-drop-folder-tree.js
@@ -53,7 +53,7 @@ Owner of DHTMLgoodies.com
 		var indicator_offsetX_sub;
 		var indicator_offsetY;
 		
-		this.imageFolder = 'images/';
+		this.imageFolder = document_root+'/skin/';
 		this.folderImage = 'dhtmlgoodies_folder.gif';
 		this.plusImage = 'dhtmlgoodies_plus.gif';
 		this.minusImage = 'dhtmlgoodies_minus.gif';
@@ -72,7 +72,7 @@ Owner of DHTMLgoodies.com
 		var helpObj;
 		
 		this.contextMenu = false;
-		this.floatingContainer = document.createElement('UL');
+		this.floatingContainer = document.createElement('ul');
 		this.floatingContainer.style.position = 'absolute';
 		this.floatingContainer.style.display='none';
 		this.floatingContainer.id = 'floatingContainer';
@@ -245,9 +245,9 @@ Owner of DHTMLgoodies.com
 		,
 		expandAll : function()
 		{
-			var menuItems = document.getElementById(this.idOfTree).getElementsByTagName('LI');
+			var menuItems = document.getElementById(this.idOfTree).getElementsByTagName('li');
 			for(var no=0;no<menuItems.length;no++){
-				var subItems = menuItems[no].getElementsByTagName('UL');
+				var subItems = menuItems[no].getElementsByTagName('ul');
 				if(subItems.length>0 && subItems[0].style.display!='block'){
 					JSTreeObj.showHideNode(false,menuItems[no].id);
 				}			
@@ -256,9 +256,9 @@ Owner of DHTMLgoodies.com
 		,
 		collapseAll : function()
 		{
-			var menuItems = document.getElementById(this.idOfTree).getElementsByTagName('LI');
+			var menuItems = document.getElementById(this.idOfTree).getElementsByTagName('li');
 			for(var no=0;no<menuItems.length;no++){
-				var subItems = menuItems[no].getElementsByTagName('UL');
+				var subItems = menuItems[no].getElementsByTagName('ul');
 				if(subItems.length>0 && subItems[0].style.display=='block'){
 					JSTreeObj.showHideNode(false,menuItems[no].id);
 				}			
@@ -271,7 +271,7 @@ Owner of DHTMLgoodies.com
 		getTopPos : function(obj){
 			var top = obj.offsetTop/1;
 			while((obj = obj.offsetParent) != null){
-				if(obj.tagName!='HTML')top += obj.offsetTop;
+				if(obj.tagName!='html')top += obj.offsetTop;
 			}			
 			if(document.all)top = top/1 + 13; else top = top/1 + 4;		
 			return top;
@@ -283,7 +283,7 @@ Owner of DHTMLgoodies.com
 		getLeftPos : function(obj){
 			var left = obj.offsetLeft/1 + 1;
 			while((obj = obj.offsetParent) != null){
-				if(obj.tagName!='HTML')left += obj.offsetLeft;
+				if(obj.tagName!='html')left += obj.offsetLeft;
 			}
 	  			
 			if(document.all)left = left/1 - 2;
@@ -295,10 +295,10 @@ Owner of DHTMLgoodies.com
 		{
 			if(inputId){
 				if(!document.getElementById(inputId))return;
-				thisNode = document.getElementById(inputId).getElementsByTagName('IMG')[0]; 
+				thisNode = document.getElementById(inputId).getElementsByTagName('img')[0]; 
 			}else {
 				thisNode = this;
-				if(this.tagName=='A')thisNode = this.parentNode.getElementsByTagName('IMG')[0];	
+				if(this.tagName=='A')thisNode = this.parentNode.getElementsByTagName('img')[0];	
 				
 			}
 			if(thisNode.style.visibility=='hidden')return;		
@@ -306,13 +306,13 @@ Owner of DHTMLgoodies.com
 			inputId = parentNode.id.replace(/[^0-9]/g,'');
 			if(thisNode.src.indexOf(JSTreeObj.plusImage)>=0){
 				thisNode.src = thisNode.src.replace(JSTreeObj.plusImage,JSTreeObj.minusImage);
-				var ul = parentNode.getElementsByTagName('UL')[0];
+				var ul = parentNode.getElementsByTagName('ul')[0];
 				ul.style.display='block';
 				if(!initExpandedNodes)initExpandedNodes = ',';
 				if(initExpandedNodes.indexOf(',' + inputId + ',')<0) initExpandedNodes = initExpandedNodes + inputId + ',';
 			}else{
 				thisNode.src = thisNode.src.replace(JSTreeObj.minusImage,JSTreeObj.plusImage);
-				parentNode.getElementsByTagName('UL')[0].style.display='none';
+				parentNode.getElementsByTagName('ul')[0].style.display='none';
 				initExpandedNodes = initExpandedNodes.replace(',' + inputId,'');
 			}	
 			JSTreeObj.Set_Cookie('dhtmlgoodies_expandedNodes',initExpandedNodes,500);			
@@ -324,7 +324,7 @@ Owner of DHTMLgoodies.com
 		{
 			if(document.all)e = event;	
 			
-			var subs = JSTreeObj.floatingContainer.getElementsByTagName('LI');
+			var subs = JSTreeObj.floatingContainer.getElementsByTagName('li');
 			if(subs.length>0){
 				if(JSTreeObj.dragNode_sourceNextSib){
 					JSTreeObj.dragNode_parent.insertBefore(JSTreeObj.dragNode_source,JSTreeObj.dragNode_sourceNextSib);
@@ -370,7 +370,7 @@ Owner of DHTMLgoodies.com
 			JSTreeObj.floatingContainer.style.top = dragDrop_y + 'px';
 			
 			var thisObj = this;
-			if(thisObj.tagName=='A' || thisObj.tagName=='IMG')thisObj = thisObj.parentNode;
+			if(thisObj.tagName=='a' || thisObj.tagName=='img')thisObj = thisObj.parentNode;
 
 			JSTreeObj.dragNode_noSiblings = false;
 			var tmpVar = thisObj.getAttribute('noSiblings');
@@ -380,14 +380,14 @@ Owner of DHTMLgoodies.com
 			if(thisObj && thisObj.id)
 			{
 				JSTreeObj.dragNode_destination = thisObj;
-				var img = thisObj.getElementsByTagName('IMG')[1];
+				var img = thisObj.getElementsByTagName('img')[1];
 				var tmpObj= JSTreeObj.dropTargetIndicator;
 				tmpObj.style.display='block';
 				
 				var eventSourceObj = this;
-				if(JSTreeObj.dragNode_noSiblings && eventSourceObj.tagName=='IMG')eventSourceObj = eventSourceObj.nextSibling;
+				if(JSTreeObj.dragNode_noSiblings && eventSourceObj.tagName=='img')eventSourceObj = eventSourceObj.nextSibling;
 				
-				var tmpImg = tmpObj.getElementsByTagName('IMG')[0];
+				var tmpImg = tmpObj.getElementsByTagName('img')[0];
 				if(this.tagName=='A' || JSTreeObj.dragNode_noSiblings){
 					tmpImg.src = tmpImg.src.replace('ind1','ind2');	
 					JSTreeObj.insertAsSub = true;
@@ -427,12 +427,12 @@ Owner of DHTMLgoodies.com
 			
 			if(JSTreeObj.dragNode_destination){			
 				if(JSTreeObj.insertAsSub){
-					var uls = JSTreeObj.dragNode_destination.getElementsByTagName('UL');
+					var uls = JSTreeObj.dragNode_destination.getElementsByTagName('ul');
 					if(uls.length>0){
 						ul = uls[0];
 						ul.style.display='block';
 						
-						var lis = ul.getElementsByTagName('LI');
+						var lis = ul.getElementsByTagName('li');
 
 						if(lis.length>0){	// Sub elements exists - drop dragable node before the first one
 							ul.insertBefore(JSTreeObj.dragNode_source,lis[0]);	
@@ -440,12 +440,12 @@ Owner of DHTMLgoodies.com
 							ul.appendChild(JSTreeObj.dragNode_source);	
 						}
 					}else{
-						var ul = document.createElement('UL');
+						var ul = document.createElement('ul');
 						ul.style.display='block';
 						JSTreeObj.dragNode_destination.appendChild(ul);
 						ul.appendChild(JSTreeObj.dragNode_source);
 					}
-					var img = JSTreeObj.dragNode_destination.getElementsByTagName('IMG')[0];					
+					var img = JSTreeObj.dragNode_destination.getElementsByTagName('img')[0];					
 					img.style.visibility='visible';
 					img.src = img.src.replace(JSTreeObj.plusImage,JSTreeObj.minusImage);					
 					
@@ -460,9 +460,9 @@ Owner of DHTMLgoodies.com
 				}	
 				/* Clear parent object */
 				var tmpObj = JSTreeObj.dragNode_parent;
-				var lis = tmpObj.getElementsByTagName('LI');
+				var lis = tmpObj.getElementsByTagName('li');
 				if(lis.length==0){
-					var img = tmpObj.parentNode.getElementsByTagName('IMG')[0];
+					var img = tmpObj.parentNode.getElementsByTagName('img')[0];
 					img.style.visibility='hidden';	// Hide [+],[-] icon
 					tmpObj.parentNode.removeChild(tmpObj);						
 				}
@@ -484,10 +484,10 @@ Owner of DHTMLgoodies.com
 		,
 		createDropIndicator : function()
 		{
-			this.dropTargetIndicator = document.createElement('DIV');
+			this.dropTargetIndicator = document.createElement('fiv');
 			this.dropTargetIndicator.style.position = 'absolute';
 			this.dropTargetIndicator.style.display='none';			
-			var img = document.createElement('IMG');
+			var img = document.createElement('img');
 			img.src = this.imageFolder + 'dragDrop_ind1.gif';
 			img.id = 'dragDropIndicatorImage';
 			this.dropTargetIndicator.appendChild(img);
@@ -500,13 +500,13 @@ Owner of DHTMLgoodies.com
 			if(direction=='up'){
 				while(obj.parentNode && obj.parentNode!=stopAtObject){
 					obj = obj.parentNode;
-					if(obj.tagName=='UL')countLevels = countLevels/1 +1;
+					if(obj.tagName=='ul')countLevels = countLevels/1 +1;
 				}		
 				return countLevels;
 			}	
 			
 			if(direction=='down'){ 
-				var subObjects = obj.getElementsByTagName('LI');
+				var subObjects = obj.getElementsByTagName('li');
 				for(var no=0;no<subObjects.length;no++){
 					countLevels = Math.max(countLevels,JSTreeObj.dragDropCountLevels(subObjects[no],"up",obj));
 				}
@@ -533,7 +533,7 @@ Owner of DHTMLgoodies.com
 				initObj = document.getElementById(this.idOfTree);
 
 			}
-			var lis = initObj.getElementsByTagName('LI');
+			var lis = initObj.getElementsByTagName('li');
 
 			if(lis.length>0){
 				var li = lis[0];
@@ -550,7 +550,7 @@ Owner of DHTMLgoodies.com
 							
 							if(li.parentNode.id!=this.idOfTree)saveString = saveString + numericParentID; else saveString = saveString + '0';
 						}
-						var ul = li.getElementsByTagName('UL');
+						var ul = li.getElementsByTagName('ul');
 						if(ul.length>0){
 							saveString = this.getNodeOrders(ul[0],saveString);	
 						}	
@@ -580,7 +580,7 @@ Owner of DHTMLgoodies.com
 		,
 		hasSubNodes : function(obj)
 		{
-			var subs = obj.getElementsByTagName('LI');
+			var subs = obj.getElementsByTagName('li');
 			if(subs.length>0)return true;
 			return false;	
 		}
@@ -590,7 +590,7 @@ Owner of DHTMLgoodies.com
 			var message = 'Click OK to delete item ' + obj2.innerHTML;
 			if(this.hasSubNodes(obj2.parentNode)) message = message + ' and it\'s sub nodes';
 			if(confirm(message)){
-				this.__deleteItem_step2(obj2.parentNode);	// Sending <LI> tag to the __deleteItem_step2 method	
+				this.__deleteItem_step2(obj2.parentNode);	// Sending <li> tag to the __deleteItem_step2 method	
 			}
 			
 		}
@@ -599,7 +599,7 @@ Owner of DHTMLgoodies.com
 		{
 			if(this.hasSubNodes(obj))return;
 
-			var img = obj.getElementsByTagName('IMG')[0];
+			var img = obj.getElementsByTagName('img')[0];
 			img.style.visibility = 'hidden';	
 		}
 		,
@@ -608,7 +608,7 @@ Owner of DHTMLgoodies.com
 
 			var saveString = obj.id.replace(/[^0-9]/gi,'');
 			
-			var lis = obj.getElementsByTagName('LI');
+			var lis = obj.getElementsByTagName('li');
 			for(var no=0;no<lis.length;no++){
 				saveString = saveString + ',' + lis[no].id.replace(/[^0-9]/gi,'');
 			}
@@ -702,7 +702,7 @@ Owner of DHTMLgoodies.com
 		,
 		__createTextBox : function(obj)
 		{
-			var textBox = document.createElement('INPUT');
+			var textBox = document.createElement('input');
 			textBox.className = 'folderTreeTextBox';
 			textBox.value = obj.innerHTML;
 			obj.parentNode.insertBefore(textBox,obj);	
@@ -742,7 +742,7 @@ Owner of DHTMLgoodies.com
 			document.documentElement.onmousedown = JSTreeObj.removeHighlight;
 			
 			/* Creating help object for storage of values */
-			this.helpObj = document.createElement('DIV');
+			this.helpObj = document.createElement('div');
 			this.helpObj.style.display = 'none';
 			document.body.appendChild(this.helpObj);
 			
@@ -776,7 +776,8 @@ Owner of DHTMLgoodies.com
 					
 			var nodeId = 0;
 			var dhtmlgoodies_tree = document.getElementById(this.idOfTree);
-			var menuItems = dhtmlgoodies_tree.getElementsByTagName('LI');	// Get an array of all menu items
+//			alert('tree '+this.idOfTree+dhtmlgoodies_tree);
+			var menuItems = dhtmlgoodies_tree.getElementsByTagName('li');	// Get an array of all menu items
 			for(var no=0;no<menuItems.length;no++){
 				// No children var set ?
 				var noChildren = false;
@@ -790,8 +791,8 @@ Owner of DHTMLgoodies.com
 				if(tmpVar=='true')noDrag=true;
 						 
 				nodeId++;
-				var subItems = menuItems[no].getElementsByTagName('UL');
-				var img = document.createElement('IMG');
+				var subItems = menuItems[no].getElementsByTagName('ul');
+				var img = document.createElement('img');
 				img.src = this.imageFolder + this.plusImage;
 				img.onclick = JSTreeObj.showHideNode;
 				
@@ -799,14 +800,14 @@ Owner of DHTMLgoodies.com
 					subItems[0].id = 'tree_ul_' + treeUlCounter;
 					treeUlCounter++;
 				}
-				var aTag = menuItems[no].getElementsByTagName('A')[0];
+				var aTag = menuItems[no].getElementsByTagName('a')[0];
 				aTag.id = 'nodeATag' + menuItems[no].id.replace(/[^0-9]/gi,'');
 				//aTag.onclick = JSTreeObj.showHideNode;
 				if(!noDrag)aTag.onmousedown = JSTreeObj.initDrag;
 				if(!noChildren)aTag.onmousemove = JSTreeObj.moveDragableNodes;
 				menuItems[no].insertBefore(img,aTag);
 				//menuItems[no].id = 'dhtmlgoodies_treeNode' + nodeId;
-				var folderImg = document.createElement('IMG');
+				var folderImg = document.createElement('img');
 				if(!noDrag)folderImg.onmousedown = JSTreeObj.initDrag;
 				folderImg.onmousemove = JSTreeObj.moveDragableNodes;
 				if(menuItems[no].className){
